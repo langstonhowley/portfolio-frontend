771c41d45817260119539966ec2b7ee7a7d70f51
Langston Howley <ljhowley11@gmail.com>
2024-03-11 11:30:25 -0400

    Update to resume

--- src/screens/online-resume/OnlineResume.js
+++ src/screens/online-resume/OnlineResume.js
@@ -3,8 +3,8 @@ import './OnlineResume.css'
 const OnlineResume = () => {
     return ( 
         <div className='resumeCont'>
-            <img src="/images/Resume1.png" alt="Resume" />
-            <img src="/images/Resume2.png" alt="Resume" />
+            <img src="/images/Resume1.jpg" alt="Resume" />
+            <img src="/images/Resume2.jpg" alt="Resume" />
         </div>
     )
 }
